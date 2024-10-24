7f853adc63858d13ac4f532d447c8a00978f08e2
Kenneth Ordeneaux <ordeneauxk@outlook.com>
2024-10-24 02:32:47 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace PokeApiApp
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var services = CreateServices();
+            Application app = services.GetRequiredService<Application>();
+            app.MyLogic();
+        }
+
+        private static ServiceProvider CreateServices()
+        {
+            var serviceProvider = new ServiceCollection()
+                .AddSingleton<Application>(new Application())
+                .AddLogging(logging=> logging.AddSimpleConsole())
+                //.AddLogging(logging => logging.AddConsole())
+                .BuildServiceProvider();
+
+            return serviceProvider;
+        }
+    }
+
+    public class Application
+    {
+        public void MyLogic()
+        {
+            Console.WriteLine("Hello World."); 
+        }
+    }
+}
